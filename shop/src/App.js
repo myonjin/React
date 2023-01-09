4a91163157ba5b2ac95d4e7d58aa93fe2efd1aff
myonjin <kjinabc@gmail.com>
2023-01-10 02:51:31 +0900

    shop

---
+++ shop/src/App.js
@@ -0,0 +1,59 @@
+import "./App.css";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import bg from "./img/bg.png";
+import { useState } from "react";
+import data from "./data.js";
+// eslint-disable-next-line
+import { Routes, Route, Link } from "react-router-dom";
+
+function App() {
+  let [shoes] = useState(data);
+
+  return (
+    <div className="App">
+      <Navbar bg="light" variant="light">
+        <Container>
+          <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
+          <Nav className="me-auto">
+            <Link to="/">홈</Link>
+            <Link to="/detail">상세페이지</Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <div className="main-bg" style={{ backgroundImage: "url(" + bg + ")" }}></div>
+              <div className="container">
+                <div className="row">
+                  {shoes.map(function (ele, i) {
+                    return <Modal shoes={shoes} index={i} />;
+                  })}
+                </div>
+              </div>
+            </>
+          }
+        />
+        <Route path="/detail" element={<div>상세페이지임</div>} />
+      </Routes>
+    </div>
+  );
+}
+
+function Modal(props) {
+  return (
+    <div className="col-md-4">
+      <img alt="" src={`https://codingapple1.github.io/shop/shoes${props.index + 1}.jpg`} width="80%" />
+      <h4>{props.shoes[props.index].title}</h4>
+      <p>{props.shoes[props.index].content}</p>
+    </div>
+  );
+}
+
+
+
+
+export default App;
